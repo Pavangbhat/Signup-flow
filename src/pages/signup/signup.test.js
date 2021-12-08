6d8b7c93f30fec762c4dd01ad200ca66cba40353
pavangbhat <pavanbhat17@gmail.com>
2021-12-08 12:28:48 +0530

    enhanced test cases

--- src/pages/signup/signup.test.js
+++ src/pages/signup/signup.test.js
@@ -3,54 +3,52 @@ import { render, fireEvent } from "@testing-library/react";
 import Signup from "./index";
 import "@testing-library/jest-dom";
 
-
-
-const setUp=(elementId)=>{
-    //TODO: Helper function set up render of component
-}
+const setup = (elementId) => {
+  const utils = render(<Signup />);
+  const input = utils.getByTestId(elementId);
+  return {input, ...utils}
+};
 
 describe("Check if all elements are present or not in sign up page", () => {
-  const { getAllByTestId } = render(<Signup />);
-  const email = getAllByTestId("email")[0];
-  const password = getAllByTestId("password")[0];
-  const confirmPassword = getAllByTestId("confirmPassword")[0];
-  const submitBtn = getAllByTestId("submitBtn")[0];
-  const passwordStrengthStatus = getAllByTestId("passwordStrengthStatus")[0];
-
   test("email be present in document", () => {
+    const {input:email} = setup('email')
     expect(email).toBeInTheDocument();
   });
 
   test("password be present in document", () => {
+    const {input:password} = setup('password')
     expect(password).toBeInTheDocument();
   });
 
   test("confirmPassword be present in document", () => {
+    const {input:confirmPassword} = setup('confirmPassword')
     expect(confirmPassword).toBeInTheDocument();
   });
 
   test("submitBtn be present in document", () => {
+    const {input:submitBtn} = setup('submitBtn')
     expect(submitBtn).toBeInTheDocument();
   });
 
   test("passwordStrengthStatus be present in document", () => {
-    expect(passwordStrengthStatus).not.toBeInTheDocument();
+    const {input:passwordStrengthStatus} = setup('passwordStrengthStatus')
+    expect(passwordStrengthStatus).toBeInTheDocument();
   });
 });
 
 describe("email", () => {
-  const { getAllByTestId } = render(<Signup />);
-  const email = getAllByTestId("email")[0];
-
   test("has required feild", () => {
+    const {input:email} = setup('email')
     expect(email).toHaveAttribute("required");
   });
 
   test("type of email to be email", () => {
+    const {input:email} = setup('email')
     expect(email).toHaveAttribute("type", "email");
   });
 
   test("border color not to be red by default", () => {
+    const {input:email} = setup('email')
     expect(email).not.toHaveClass("borderRed");
   });
 
@@ -59,6 +57,7 @@ describe("email", () => {
   test.todo("test email validator when email is not valid", () => {});
 
   test("border color to be red when invaild email", () => {
+    const {input:email} = setup('email')
     fireEvent.change(email, { target: { value: "invalidemail" } });
     expect(email.classList.contains("borderRed")).toBe(true);
 
@@ -70,22 +69,22 @@ describe("email", () => {
   });
 
   test("border color not to be red if vaild email", () => {
+    const {input:email} = setup('email')
     fireEvent.change(email, { target: { value: "valid@d.com" } });
     expect(email.classList.contains("borderRed")).toBe(false);
   });
 });
 
 describe("password", () => {
-  const { getAllByTestId } = render(<Signup />);
-  const password = getAllByTestId("password")[0];
-
   test("type of be password", () => {
+    const {input:password} = setup('password')
     expect(password).toHaveAttribute("type", "password");
   });
 
   test.todo("password validator", () => {});
 
   test("border not be red when password is empty string or password is greater than or equal to 5", () => {
+    const {input:password} = setup('password')
     fireEvent.change(password, { target: { value: "" } });
     expect(password.classList.contains("borderRed")).toBe(false);
 
@@ -94,6 +93,7 @@ describe("password", () => {
   });
 
   test("border red when password is less than 5 character", () => {
+    const {input:password} = setup('password')
     fireEvent.change(password, { target: { value: "2d3" } });
     expect(password.classList.contains("borderRed")).toBe(true);
 
@@ -103,26 +103,22 @@ describe("password", () => {
 });
 
 describe("confirm password", () => {
-  const { getAllByTestId } = render(<Signup />);
-  const confirmPassword = getAllByTestId("confirmPassword")[0];
-
   test("type of be password", () => {
+    const {input:confirmPassword} = setup('confirmPassword')
     expect(confirmPassword).toHaveAttribute("type", "password");
   });
 
   test("border red if password doesnot match", () => {
-    const { getAllByTestId } = render(<Signup />);
-    const password = getAllByTestId("password")[0];
-
+    const {input:password} = setup('password')
+    const {input:confirmPassword} = setup('confirmPassword')
     fireEvent.change(password, { target: { value: "h22se" } });
     fireEvent.change(confirmPassword, { target: { value: "h21se" } });
     expect(confirmPassword.classList.contains("borderRed")).toBe(true);
   });
 
   test("not to have border red if password matches", () => {
-    const { getAllByTestId } = render(<Signup />);
-    const password = getAllByTestId("password")[0];
-
+    const {input:password} = setup('password')
+    const {input:confirmPassword} = setup('confirmPassword')
     fireEvent.change(password, { target: { value: "h22se" } });
     fireEvent.change(confirmPassword, { target: { value: "h22se" } });
     expect(confirmPassword.classList.contains("borderRed")).toBe(false);
@@ -130,68 +126,53 @@ describe("confirm password", () => {
 });
 
 describe("status label", () => {
-  const { getAllByTestId } = render(<Signup />);
-  const passwordStrengthStatus = getAllByTestId("passwordStrengthStatus")[0];
-
   test("if password is empty string label should not be visible", () => {
-    const { getAllByTestId } = render(<Signup />);
-    const password = getAllByTestId("password")[0];
-
+    const {input:passwordStrengthStatus} = setup('passwordStrengthStatus')
+    const {input:password} = setup('password')
     fireEvent.change(password, { target: { value: "" } });
     expect(passwordStrengthStatus).not.toBeInTheDocument();
   });
 
-  test.todo("status label determiner",()=>{
+  test.todo("status label determiner", () => {});
 
-  })
-
-  test("label to have string of Weak if password is weak",()=>{
-    const { getAllByTestId } = render(<Signup />);
-    const password = getAllByTestId("password")[0];
-
+  test("label to have string of Weak if password is weak", () => {
+    const {input:passwordStrengthStatus} = setup('passwordStrengthStatus')
+    const {input:password} = setup('password')
     fireEvent.change(password, { target: { value: "dea" } });
-    expect(passwordStrengthStatus).toHaveTextContent('Weak')
-  })
+    expect(passwordStrengthStatus).toHaveTextContent("Weak");
+  });
 
-  test("label to have string of Medium if password is weak",()=>{
-    const { getAllByTestId } = render(<Signup />);
-    const password = getAllByTestId("password")[0];
-
+  test("label to have string of Medium if password is weak", () => {
+    const {input:passwordStrengthStatus} = setup('passwordStrengthStatus')
+    const {input:password} = setup('password')
     fireEvent.change(password, { target: { value: "deawf" } });
-    expect(passwordStrengthStatus).toHaveTextContent('Medium')
-  })
+    expect(passwordStrengthStatus).toHaveTextContent("Medium");
+  });
 
-  test("label to have string of Strong if password is weak",()=>{
-    const { getAllByTestId } = render(<Signup />);
-    const password = getAllByTestId("password")[0];
-
+  test("label to have string of Strong if password is weak", () => {
+    const {input:passwordStrengthStatus} = setup('passwordStrengthStatus')
+    const {input:password} = setup('password')
     fireEvent.change(password, { target: { value: "deadfres" } });
-    expect(passwordStrengthStatus).toHaveTextContent('Strong')
-  })
-
-
+    expect(passwordStrengthStatus).toHaveTextContent("Strong");
+  });
 });
 
 describe("submit button", () => {
-  const { getAllByTestId } = render(<Signup />);
-  const submitBtn = getAllByTestId("submitBtn")[0];
-  
-  test('submit button to be disabled by default',()=>{
-      expect(submitBtn).toHaveTextContent('disabled')
-  })
+  const {input:submitBtn} = setup('submitBtn')
+  test("submit button to be disabled by default", () => {
+    expect(submitBtn).toHaveTextContent("disabled");
+  });
 
-  test.todo('if password  or email is not vaild button to be disabled',()=>{
+  test.todo(
+    "if password  or email is not vaild button to be disabled",
+    () => {}
+  );
 
-  })
-
-  test.todo('if password does not match button to be disabled',()=>{
-    const { getAllByTestId } = render(<Signup />);
-    const password = getAllByTestId("password")[0];
-    const confirmPassword = getAllByTestId("password")[0];
-
+  test.todo("if password does not match button to be disabled", () => {
+    const {input:password} = setup('password')
+    const {input:confirmPassword} = setup('confirmPassword')
     fireEvent.change(password, { target: { value: "h22se" } });
     fireEvent.change(confirmPassword, { target: { value: "h21se" } });
-    expect(submitBtn).toHaveTextContent('disabled')
-  })
-
+    expect(submitBtn).toHaveTextContent("disabled");
+  });
 });
