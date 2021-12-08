cd5c86ba09c42a418ac05fff317bb9722588c143
pavangbhat <pavanbhat17@gmail.com>
2021-12-08 13:34:06 +0530

    implemented router v6

---
+++ src/pages/pageNotFound/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./style.css"
+
+const PageNotFound = () => {
+    return (
+        <div>
+            <span>404 Page Not Found</span>
+        </div>
+    )
+}
+
+export default PageNotFound
