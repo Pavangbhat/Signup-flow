df9f9c87afcf629298ba1418fbee5909b6d83b5b
pavangbhat <pavanbhat17@gmail.com>
2021-12-08 12:41:37 +0530

    implemented UI for Signup page

---
+++ src/components/cardHeading/CardHeading.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./style.css"
+
+const CardHeading = ({heading}) => {
+    return (
+        <div className="heading">
+            {heading}
+        </div>
+    )
+}
+
+export default CardHeading
--- src/pages/signup/index.js
+++ src/pages/signup/index.js
@@ -1,12 +1,43 @@
-import React from 'react'
+import React from "react";
+import CardHeading from "../../components/cardHeading/CardHeading";
+import "./style.css";
 
 const Signup = () => {
-    return (
-        <React.Fragment>
-            Signup page
-            <input data-testid="email"/>
-        </React.Fragment>
-    )
-}
+  return (
+    <React.Fragment>
+      <div className="signup">
+        <CardHeading heading="Signup" />
+        <form
+          className="inputForm"
+          onSubmit={(e) => {
+            e.preventDefault();
+          }}
+        >
+          <div>
+            <div className="inputWrapper">
+              <label>Email</label>
+              <input placeholder="eg:bhat@gmail.com" data-testid="email"/>
+            </div>
+            <div className="inputWrapper">
+              <label>Password</label>
+              <input placeholder="Enter your password" data-testid="password"/>
+            </div>
+            <div className="passwordStrength">
+              Password Strength:<span data-testid="passwordStrengthStatus">Weak</span>
+            </div>
+            <div className="inputWrapper">
+              <label>Confirm Password</label>
+              <input placeholder="Re-enter your password" data-testid="confirmPassword"/>
+            </div>
+          </div>
 
-export default Signup
+          <button className="submitBtn" data-testid="submitBtn">
+              Submit
+          </button>
+        </form>
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default Signup;
