45309e45a544f757d1618b051dfc201c0e19445f
pavangbhat <pavanbhat17@gmail.com>
2021-12-12 00:11:03 +0530

    wrote test cases for referral page

---
+++ src/pages/referral/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./style.css"
+
+const Referral = () => {
+    return (
+        <div>
+            Referral page
+        </div>
+    )
+}
+
+export default Referral
