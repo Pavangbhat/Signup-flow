d8f8b6995091fa6221e5d83b652430ef47e35e5b
pavangbhat <pavanbhat17@gmail.com>
2021-12-13 15:25:57 +0530

    implemented waiting list page

--- src/pages/waitingList/index.js
+++ src/pages/waitingList/index.js
@@ -2,7 +2,12 @@ import React from "react";
 import "./style.css";
 
 const WaitingList = () => {
-  return <div>Waiting List</div>;
+  return (
+    <div data-testid="cardWrapper" className="cardWrapper">
+      <p>You are added to the waiting list !!</p>
+      <p>Only 34 people ahead of you</p>
+    </div>
+  );
 };
 
 export default WaitingList;
